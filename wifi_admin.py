c4d513d5aa321ecdf4f48c9b1b9d5fd413bad3bd
James <pavja2@gmail.com>
2016-04-09 10:04:17 +0000

    Worked on checking wifi admin through scans

--- wifi_admin.py
+++ wifi_admin.py
@@ -2,6 +2,16 @@
 # which returns a value from 0 to 100, inclusive
 # 100 is the best score, 0 is the worst
 
-class Sample_Scan:
+import netifaces
+import requests
+
+def get_default_gateway():
+	return netifaces.gateways()['default'][netifaces.AF_INET][0]	
+
+class Wifi_Admin_Scan:
     def run_scan(self):
+	gateway = get_default_gateway()
+	if(len(gateway) > 0):
+		r = requests.get('http://' + gateway, verify=False)
+		print r.status_code
         return 100 # way hella
