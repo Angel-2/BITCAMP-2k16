aa439c33c9e3686c936e33182ea6d0fb7e163383
casey <caseyknerr@gmail.com>
2016-04-09 11:45:02 -0400

    THINGS

--- application.py
+++ application.py
@@ -1,15 +1,26 @@
 from driver import get_total_scan_score
 
 from flask import Flask, render_template, url_for
+from flask_bootstrap import Bootstrap
 
-app = Flask(__name__)
+def create_app():
+	app = Flask(__name__)
+	Bootstrap(app)
+	return app
+
+app = create_app()
 
 ROOT_TEMPLATE = 'root.html'
+LOAD_TEMPLATE = 'load.html'
 SCAN_TEMPLATE = 'scan.html'
 
 @app.route('/')
 def redirect_root():
     return render_template(ROOT_TEMPLATE)
+
+@app.route('/load')
+def redirect_load():
+    return render_template(LOAD_TEMPLATE)
 
 @app.route('/scan')
 def redirect_scan():
